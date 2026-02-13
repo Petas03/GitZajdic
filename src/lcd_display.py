6de8245e0c3bf424c872ce52a79b2085a37caed9
Zajdic <zajicon23@sps-prosek.cz>
2026-02-13 13:04:04 +0100

    Add LCD display control functions

---
+++ src/lcd_display.py
@@ -0,0 +1,15 @@
+# LCD Display Control Functions
+
+def initialize_lcd():
+    """Initialize the LCD display."""
+    pass
+
+
+def display_message(message):
+    """Display a message on the LCD."""
+    pass
+
+
+def clear_display():
+    """Clear the LCD display."""
+    pass
